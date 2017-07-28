97321404f810a8d075bd89d5467d7b2fb453ff7a
DeveloperTUNA <developertuna@gmail.com>
2017-07-28 23:15:27 +0900

    170728
    Mastering Algorithms with C
    Graph Header 추가

---
+++ Mastering_Algorithms_with_C/Mastering_Algorithms_with_C/B_Graph/graph.h
@@ -0,0 +1,36 @@
+#ifndef GRAPH_H
+#define GRAPH_H
+
+#include <stdlib.h>
+#include "../5_Linked_List/list.h"
+#include "../7_Sets/set.h"
+
+typedef struct AdjList_
+{
+	void *vertex;
+	Set Adjacent;
+}AdjList;
+
+typedef struct Graph_
+{
+	int n_Vcount;
+	int n_Ecount;
+	int(*match)(const void *key1, const void *key2);
+	int(*destory)(void *data);
+	List AdjLists;
+}Graph;
+
+typedef enum VertexColor_ { white, gray, black } VertexColor;
+void GraphInit(Graph *graph, int(*match)(const void *key1, const void *key2), void(*destory)(void *data));
+void GraphDestory(Graph *graph);
+int GraphInsert_Vertex(Graph *graph, const void *data);
+int GraphInsert_Edge(Graph *graph, const void *data1, const void *data2);
+int GraphRemove_Vertex(Graph *graph, void **data);
+int GraphRemove_Edge(Graph *graph, void *data1, void **data2);
+int GraphAdjList(const Graph *graph, const void *data, AdjList **adjlist);
+int GraphIsAdjacent(const Graph *graph, const void *data1, const void *data2);
+#define GraphAdjLists(graph)((graph)->AdjLists)
+#define GraphVcount(graph)(graph)->n_Vcount)
+#define GraphEcount(graph)(graph)->n_Ecount)
+
+#endif GRAPH_H
