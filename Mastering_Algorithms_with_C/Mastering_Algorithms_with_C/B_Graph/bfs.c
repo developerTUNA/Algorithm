832b50f34f11f912858e9a4c02202743decf8a53
DeveloperTUNA <developertuna@gmail.com>
2017-08-07 22:40:14 +0900

    170807
    Mastering Algorithms with C
    bfs.c 완료

--- Mastering_Algorithms_with_C/Mastering_Algorithms_with_C/B_Graph/bfs.c
+++ Mastering_Algorithms_with_C/Mastering_Algorithms_with_C/B_Graph/bfs.c
@@ -37,9 +37,61 @@ int BFS(Graph *graph, BFSVertex *start, List *hops)
 	}
 	if (QueueEnqueue(&queue, adjlist_Clear) != 0)
 	{
-		QueueDestory(&queue);
+		QueueDestroy(&queue);
 		return -1;
 	}
 
+	while (QueueSize(&queue) > 0)
+	{
+		adjlist = QueuePeek(&queue);
+		for (member = ListHead(&adjlist->Adjacent); member != NULL; member = ListNext(member))
+		{
+			vertex_Adj = ListData(member);
 
+			if (GraphAdjList(graph, vertex_Adj, &adjlist_Clear))
+			{
+				QueueDestroy(&queue);
+				return -1;
+			}
+			vertex_Clear = adjlist_Clear->vertex;
+
+			if (vertex_Clear->color == white)
+			{
+				vertex_Clear->color = gray;
+				vertex_Clear->nHops = ((BFSVertex*)adjlist->vertex)->nHops + 1;
+
+				if (QueueEnqueue(&queue, adjlist_Clear) != 0)
+				{
+					QueueDestroy(&queue);
+					return -1;
+				}
+			}
+		}
+		if (QueueDequeue(&queue, (void**)&adjlist) == 0)
+		{
+			((BFSVertex*)adjlist->vertex)->color = black;
+		}
+		else
+		{
+			QueueDestroy(&queue);
+			return -1;
+		}
+	}
+	QueueDestroy(&queue);
+
+	ListInit(hops, NULL);
+	for (element = ListHead(&GraphAdjLists(graph)); element != NULL; element = ListNext(element))
+	{
+		vertex_Clear = ((AdjList*)ListData(element))->vertex;
+		if (vertex_Clear->nHops != -1)
+		{
+			if (ListInsert_Next(hops, ListTail(hops), vertex_Clear) != 0)
+			{
+				ListDestory(hops);
+				return -1;
+			}
+		}
+	}
+
+	return 0;
 }
