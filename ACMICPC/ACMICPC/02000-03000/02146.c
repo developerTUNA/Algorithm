6558f0c6cffc95c89ae6f31f4ca275396ceacc11
DeveloperTUNA <developertuna@gmail.com>
2017-09-01 23:23:52 +0900

    170901
    ACMICPC
    완료 : 02146

--- ACMICPC/ACMICPC/02000-03000/02146.c
+++ ACMICPC/ACMICPC/02000-03000/02146.c
@@ -63,6 +63,52 @@ int StackPop02146(Stack02146 *stack, int *nRow, int *nCol)
 	stack->top = stack->top->next;
 	stack->nSize--;
 	free(delNode);
+
+	return 0;
+}
+
+int QueuePush02146(Queue02146 *queue, int nRow, int nCol)
+{
+	Node02146 *newNode = NULL;
+	newNode = (Node02146*)malloc(sizeof(Node02146));
+	newNode->data.nRow = nRow;
+	newNode->data.nCol = nCol;
+	newNode->next = NULL;
+
+	if (queue->nSize == 0)
+	{
+		queue->first = newNode;
+		queue->last = newNode;
+	}
+	else
+	{
+		queue->last->next = newNode;
+		queue->last = newNode;
+	}
+	queue->nSize++;
+	return 0;
+}
+int QueuePop02146(Queue02146 *queue, int *nRow, int *nCol)
+{
+	Node02146 *delNode = NULL;
+	
+	if (queue->nSize == 0)
+	{
+		return -1;
+	}
+
+	delNode = queue->first;
+	*nRow = delNode->data.nRow;
+	*nCol = delNode->data.nCol;
+
+	queue->first = queue->first->next;
+	if (queue->nSize == 1)
+	{
+		queue->last = NULL;
+	}
+	queue->nSize--;
+
+	free(delNode);
 	return 0;
 }
 int GroupMake02146(int nSize, int **pp_nMap_Org, int **pp_nMap_Group)
@@ -70,7 +116,7 @@ int GroupMake02146(int nSize, int **pp_nMap_Org, int **pp_nMap_Group)
 	int nCount = 0;
 	int a_nMoveRow[4] = { 0,0,1,-1 };
 	int a_nMoveCol[4] = { 1,-1,0,0 };
-	Stack02146 *stack;
+	Stack02146 *stack = NULL;
 	stack = (Stack02146*)malloc(sizeof(Stack02146));
 	memset(stack, 0, sizeof(Stack02146));
 	for (int i = 0; i < nSize; i++)
@@ -113,10 +159,83 @@ int GroupMake02146(int nSize, int **pp_nMap_Org, int **pp_nMap_Group)
 		}
 	}
 	free(stack);
+	return nCount;
+}
+int GroupCalc02146(int nSize, int nGroupCount, int **pp_nMap_Group, int **pp_nMap_Bridge)
+{
+	int nResult = 0;
+	int a_nMoveRow[4] = { 0,0,1,-1 };
+	int a_nMoveCol[4] = { 1,-1,0,0 };
+	Queue02146 *queue = NULL;
+	queue = (Queue02146*)malloc(sizeof(Queue02146));
+	memset(queue, 0, sizeof(Queue02146));
+	
+	nResult = nSize;
+	for (int i = 0; i < nGroupCount; i++)
+	{
+		for (int j = 0; j < nSize; j++)
+		{
+			memset(pp_nMap_Bridge[j], 0, sizeof(int)*nSize);
+		}
+		for (int j = 0; j < nSize; j++)
+		{
+			for (int k = 0; k < nSize; k++)
+			{
+				if (pp_nMap_Group[j][k] == i + 1)
+				{
+					QueuePush02146(queue, j, k);
+					pp_nMap_Bridge[j][k] = 1;
+				}
+			}
+		}
+		printf("\n");
+		PrintIntArray2(pp_nMap_Bridge, 0, nSize - 1, 0, nSize - 1);
+		int nBridge = 0;
+		while (0 < queue->nSize)
+		{
+			int nGetRow = 0;
+			int nGetCol = 0;
+
+			QueuePop02146(queue, &nGetRow, &nGetCol);
+			for (int j = 0; j < 4; j++)
+			{
+				int nCheckRow = nGetRow + a_nMoveRow[j];
+				int nCheckCol = nGetCol + a_nMoveCol[j];
+				if (nCheckRow < 0 || nSize <= nCheckRow ||
+					nCheckCol < 0 || nSize <= nCheckCol)
+				{
+					continue;
+				}
+				if (pp_nMap_Bridge[nCheckRow][nCheckCol] == 0&&
+					pp_nMap_Group[nCheckRow][nCheckCol] == 0)//||
+					//pp_nMap_Bridge[nGetRow][nGetCol] + 1 < pp_nMap_Bridge[nCheckRow][nCheckCol])
+				{
+					pp_nMap_Bridge[nCheckRow][nCheckCol] = pp_nMap_Bridge[nGetRow][nGetCol] + 1;
+					printf("\n");
+					PrintIntArray2(pp_nMap_Bridge, 0, nSize - 1, 0, nSize - 1);
+					QueuePush02146(queue, nCheckRow, nCheckCol);
+				}
+
+				if (pp_nMap_Group[nCheckRow][nCheckCol] != i + 1 &&
+					pp_nMap_Group[nCheckRow][nCheckCol] != 0)
+				{
+					nBridge = pp_nMap_Bridge[nGetRow][nGetCol];
+					if (nBridge < nResult)
+					{
+						nResult = nBridge;
+					}
+				}
+			}
+		}
+	}
+	free(queue);
+	return nResult;
 }
 int Problem02146(void)
 {
 	int nSize = 0;
+	int nGroupCount = 0;
+	int nBridge = 0;
 	int **pp_nMap_Org = NULL;
 	int **pp_nMap_Group = NULL;
 	int **pp_nMap_Bridge = NULL;
@@ -143,7 +262,11 @@ int Problem02146(void)
 		}
 	}
 
-	GroupMake02146(nSize, pp_nMap_Org, pp_nMap_Group);
+	nGroupCount = GroupMake02146(nSize, pp_nMap_Org, pp_nMap_Group);
+	printf("\n");
+	PrintIntArray2(pp_nMap_Group, 0, nSize - 1, 0, nSize - 1);
+	nBridge = GroupCalc02146(nSize, nGroupCount, pp_nMap_Group, pp_nMap_Bridge);
+	printf("%d\n", nBridge-1);
 	
 	for (int i = 0; i < nSize; i++)
 	{
